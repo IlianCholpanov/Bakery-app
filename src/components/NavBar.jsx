5156d8546b8d3900cc117e8dd6480e8099a8a6da
Ilian Cholpanov <iliancholpanov@gmail.com>
2025-01-10 15:39:35 +0200

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 function NavBar({ onNavigate }) {
   return (
-    <nav className="sticky top-0 bg-white z-50 shadow-md">
-      <ul className="flex justify-around py-4">
+    <nav className="sticky backdrop-filter backdrop-blur-[10px] absolute-top-[0] left-[0] w-full h-screen translate-x-full flex items-center justify-center [transition:all_0.5s_ease-in]">
+      <ul className="flex items-center gap-[4.8rem]">
         <li>About us</li>
         <li>
           <button
